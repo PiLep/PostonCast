a417c31a03537ab63033f922050f4bb0967c8125
PiLe <contact@bananapower.xyz>
2017-07-12 10:50:15 +0200

    first commit

--- memo.rb
+++ memo.rb
@@ -1,9 +1,23 @@
+
+
+#### Non necessaire
 rails new PostonCast -d mysql 
+
+
+### Necessaire
 cd PostonCast/
+gem install bundler
+bundle install
 rake db:create
+
+
+#### Non necessaire
 rails generate scaffold User nom:string prenom:string email:string pwd:string
 rails generate scaffold Podcasts genre:string description:string image:string fichier:string
 rails generate scaffold Playlists nomPlaylist:string
 rails generate scaffold GroupePodcasts nomGroupe:string prenom:string email:string pwd:string
+
+
+### Necessaire
 rake db:migrate
 
